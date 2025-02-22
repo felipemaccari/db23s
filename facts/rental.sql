c260866a3755b7e7bd0ab6982a1eaf3ab9897d82
Felipe Cesar Maccari <felipecesarmaccari@gmail.com>
2025-02-22 17:04:40 -0300

    add analysis 1 and 2

--- facts/rental.sql
+++ facts/rental.sql
@@ -2,8 +2,7 @@
 DROP TABLE IF EXISTS FT_RENTAL;
 
 -- CRIAÇÃO DA TABELA FATO FT_RENTAL
-CREATE TABLE FT_RENTAL
-(
+CREATE TABLE FT_RENTAL (
     TIME INT NOT NULL,               -- Chave Tempo
     FILM_ID INT NULL,                -- Chave do Filme
     CATEGORY_ID INT NULL,            -- Chave da Categoria
@@ -11,8 +10,11 @@ CREATE TABLE FT_RENTAL
     STAFF_ID INT NULL,               -- Chave do Funcionário
     STORE_ID INT NULL,               -- Chave da Loja
     INVENTORY_ID INT NULL,           -- Chave do Inventário
+    ACTOR_ID INT NULL,               -- Chave do Ator (nova coluna)
     QUANTITY INT NOT NULL,           -- Quantidade de Aluguéis
-    AMOUNT DECIMAL(10, 2) NOT NULL,  -- Valor Total do Aluguel
+    AMOUNT DECIMAL(10, 2) NOT NULL DEFAULT 0.00,  -- Valor Total do Aluguel
+    STATUS_PAGAMENTO VARCHAR(20),    -- Status do Pagamento (Pago/Não Pago)
+    LUCRO_PREJUIZO DECIMAL(10, 2)    -- Lucro ou Prejuízo
 );
 
 -- Verificar se a Procedure já existe, se SIM, fazer o DROP e recriar
@@ -27,8 +29,8 @@ IF EXISTS (
 CREATE PROCEDURE DBO.PROC_ETL_FT_RENTAL
 AS
 BEGIN
-  -- Variáveis de controle
-  DECLARE 
+-- Variáveis de controle
+  DECLARE
     @TIME INT,
     @FILM_ID INT,
     @CATEGORY_ID INT,
@@ -36,11 +38,14 @@ BEGIN
     @STAFF_ID INT,
     @STORE_ID INT,
     @INVENTORY_ID INT,
+    @ACTOR_ID INT,
     @QUANTITY INT,
     @AMOUNT DECIMAL(10, 2),
+    @STATUS_PAGAMENTO VARCHAR(20),
+    @LUCRO_PREJUIZO DECIMAL(10, 2),
     @V_DSC_DADOS_PROCESSAMENTO VARCHAR(2000);
 
-  -- Declaração do Cursor para capturar os dados do select
+    -- Declaração do Cursor para capturar os dados do select
   DECLARE CUR_GET_RENTAL CURSOR FOR
   WITH TESTE AS (
     SELECT
@@ -51,8 +56,11 @@ BEGIN
         r.STAFF_ID,
         i.STORE_ID,
         r.INVENTORY_ID,
+        fa.ACTOR_ID,
         COALESCE(COUNT(r.rental_id), 0) AS QUANTITY,
-        COALESCE(SUM(p.amount), 0) AS AMOUNT
+        COALESCE(SUM(p.amount), 0) AS AMOUNT,
+        CASE WHEN p.payment_id IS NULL THEN 'Não Pago' ELSE 'Pago' END AS STATUS_PAGAMENTO,
+        CASE WHEN p.payment_id IS NULL THEN -f.replacement_cost ELSE p.amount END AS LUCRO_PREJUIZO
     FROM DIM_TEMPO dt
     LEFT JOIN RENTAL r
         ON YEAR(r.RENTAL_DATE) = dt.DIM_TEMPO_ANO
@@ -65,36 +73,37 @@ BEGIN
         ON f.FILM_ID = fc.FILM_ID
     LEFT JOIN DIM_CATEGORY cat
         ON fc.CATEGORY_ID = cat.CATEGORY_ID
+    LEFT JOIN FILM_ACTOR fa
+        ON f.FILM_ID = fa.FILM_ID
     LEFT JOIN PAYMENT p
         ON r.RENTAL_ID = p.RENTAL_ID
-    WHERE (dt.DIM_TEMPO_MES IS NULL AND dt.DIM_TEMPO_ANO IS NOT NULL)
-       OR (dt.DIM_TEMPO_MES IS NOT NULL AND dt.DIM_TEMPO_ANO IS NOT NULL)
-    GROUP BY dt.DIM_TEMPO_SEQ_TEMPO, f.FILM_ID, cat.CATEGORY_ID, r.CUSTOMER_ID, r.STAFF_ID, i.STORE_ID, r.INVENTORY_ID
+    GROUP BY dt.DIM_TEMPO_SEQ_TEMPO, f.FILM_ID, cat.CATEGORY_ID, r.CUSTOMER_ID, r.STAFF_ID, i.STORE_ID, r.INVENTORY_ID, fa.ACTOR_ID, p.payment_id, f.replacement_cost, p.amount
   )
-  SELECT TIME, FILM_ID, CATEGORY_ID, CUSTOMER_ID, STAFF_ID, STORE_ID, INVENTORY_ID, QUANTITY, AMOUNT
+  SELECT TIME, FILM_ID, CATEGORY_ID, CUSTOMER_ID, STAFF_ID, STORE_ID, INVENTORY_ID, ACTOR_ID, QUANTITY, AMOUNT, STATUS_PAGAMENTO, LUCRO_PREJUIZO
   FROM TESTE;
 
   -- Abrindo o cursor
   OPEN CUR_GET_RENTAL;
-  
-  -- Captura do primeiro registro
-  FETCH NEXT FROM CUR_GET_RENTAL INTO @TIME, @FILM_ID, @CATEGORY_ID, @CUSTOMER_ID, @STAFF_ID, @STORE_ID, @INVENTORY_ID, @QUANTITY, @AMOUNT;
 
-  -- Loop para processar todos os registros
+  -- Captura do primeiro registro
+  FETCH NEXT FROM CUR_GET_RENTAL INTO @TIME, @FILM_ID, @CATEGORY_ID, @CUSTOMER_ID, @STAFF_ID, @STORE_ID, @INVENTORY_ID, @ACTOR_ID, @QUANTITY, @AMOUNT, @STATUS_PAGAMENTO, @LUCRO_PREJUIZO;
+
+-- Loop para processar todos os registros
   WHILE (@@FETCH_STATUS = 0)
   BEGIN
-    -- Informações de processamento
+  -- Informações de processamento
     SET @V_DSC_DADOS_PROCESSAMENTO = 'TIME ' + CAST(@TIME AS VARCHAR) + ', FILM ' + ISNULL(CAST(@FILM_ID AS VARCHAR), 'NULL') 
       + ', CATEGORY ' + ISNULL(CAST(@CATEGORY_ID AS VARCHAR), 'NULL') 
       + ', CUSTOMER ' + ISNULL(CAST(@CUSTOMER_ID AS VARCHAR), 'NULL') 
       + ', STAFF ' + ISNULL(CAST(@STAFF_ID AS VARCHAR), 'NULL')
       + ', STORE ' + ISNULL(CAST(@STORE_ID AS VARCHAR), 'NULL') 
-      + ', INVENTORY ' + ISNULL(CAST(@INVENTORY_ID AS VARCHAR), 'NULL');
-    
+      + ', INVENTORY ' + ISNULL(CAST(@INVENTORY_ID AS VARCHAR), 'NULL')
+      + ', ACTOR ' + ISNULL(CAST(@ACTOR_ID AS VARCHAR), 'NULL');
+
     BEGIN TRANSACTION;
-      -- Inserir os dados na tabela FT_RENTAL
-      INSERT INTO FT_RENTAL (TIME, FILM_ID, CATEGORY_ID, CUSTOMER_ID, STAFF_ID, STORE_ID, INVENTORY_ID, QUANTITY, AMOUNT)
-      VALUES (@TIME, @FILM_ID, @CATEGORY_ID, @CUSTOMER_ID, @STAFF_ID, @STORE_ID, @INVENTORY_ID, @QUANTITY, @AMOUNT);
+    -- Inserir os dados na tabela FT_RENTAL
+      INSERT INTO FT_RENTAL (TIME, FILM_ID, CATEGORY_ID, CUSTOMER_ID, STAFF_ID, STORE_ID, INVENTORY_ID, ACTOR_ID, QUANTITY, AMOUNT, STATUS_PAGAMENTO, LUCRO_PREJUIZO)
+      VALUES (@TIME, @FILM_ID, @CATEGORY_ID, @CUSTOMER_ID, @STAFF_ID, @STORE_ID, @INVENTORY_ID, @ACTOR_ID, @QUANTITY, @AMOUNT, @STATUS_PAGAMENTO, @LUCRO_PREJUIZO);
 
       -- Tratamento de erro
       IF @@ERROR <> 0
@@ -108,10 +117,10 @@ BEGIN
     COMMIT;
 
     -- Capturar próximo registro
-    FETCH NEXT FROM CUR_GET_RENTAL INTO @TIME, @FILM_ID, @CATEGORY_ID, @CUSTOMER_ID, @STAFF_ID, @STORE_ID, @INVENTORY_ID, @QUANTITY, @AMOUNT;
+    FETCH NEXT FROM CUR_GET_RENTAL INTO @TIME, @FILM_ID, @CATEGORY_ID, @CUSTOMER_ID, @STAFF_ID, @STORE_ID, @INVENTORY_ID, @ACTOR_ID, @QUANTITY, @AMOUNT, @STATUS_PAGAMENTO, @LUCRO_PREJUIZO;
   END
 
-  -- Fechar cursor e liberar recursos
+-- Fechar cursor e liberar recursos
   CLOSE CUR_GET_RENTAL;
   DEALLOCATE CUR_GET_RENTAL;
 END;
